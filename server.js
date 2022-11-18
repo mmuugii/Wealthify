1d1a86bc33e428c6f5a6150d548f98adcc056782
Muugii Munkhbold <mmunkhbold94@gmail.com>
2022-11-17 20:38:45 -0700

    added comments and updated server file

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+// Project 2 by Muugii, Cassidy, Brandon and Tim
+// Created on November 17, 2022
+// base server file 
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('listening at 3000'));
+
 app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Now listening at ${PORT}, Let's Go!`)
+});
