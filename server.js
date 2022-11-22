78051d4f09cedd961da3d3316618cf982611df43
Muugii Munkhbold <mmunkhbold94@gmail.com>
2022-11-21 19:35:36 -0700

    updated and merged with provided code-base

--- server.js
+++ server.js
@@ -1,13 +1,53 @@
 // Project 2 by Muugii, Cassidy, Brandon and Tim
 // Created on November 17, 2022
-// base server file 
+// base server file
 
 const express = require('express');
 const app = express();
+const path = require('path');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+app.use(session(sess));
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
 
 app.listen(PORT, () => {
-    console.log(`Now listening at ${PORT}, Let's Go!`)
+  console.log(`Now listening at ${PORT}, Let's Go!`);
+});
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log(`Now listening on Port ${PORT}, Let's Go!`)
+  );
 });
