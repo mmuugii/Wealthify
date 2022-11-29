3115aad6215dbd85e3b67de59e6bcc6d6ff8fe65
Mntnbassfan <108299929+Mntnbassfan@users.noreply.github.com>
2022-11-28 19:58:02 -0700

    completed Espenses.js

--- models/Expenses.js
+++ models/Expenses.js
@@ -1,2 +1,47 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
+class Expenses extends Model {}
+
+Expenses.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+
+        housing: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+        food: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+        utilities: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'user',
+              key: 'id',
+    },
+    
+
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'expenses'
+    }
+);
+
+module.exports = Expenses;
