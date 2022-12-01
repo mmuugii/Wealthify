377a81f90ce5b83ad3f41f8553e173cbec0eebc7
DoengesBA <110855912+DoengesBA@users.noreply.github.com>
2022-11-30 20:36:02 -0700

    Profile + CSS styling

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+const menu = document.getElementById('menu');
+
+Array.from(document.getElementsByClassName('menu-item')).forEach(
+  (item, index) => {
+    item.onmouseover = () => {
+      menu.dataset.activeIndex = index;
+    };
+  }
+);
+
+const inputText = document.querySelector("#input");
+const button = document.querySelector('button');
+
+const grossMonthlyIncome = () => {
+   button.innerHTML = inputText.value;
